ccde4b01a82ccbbbed5a17c8b4469982b9aecb6d
Miabek1234 <mabeksantic@gmail.com>
2026-03-13 21:51:37 +0200

    Create main Spring Boot application class RuwengTrainingCenterApplication.java

---
+++ ruweng-ctc-backend/src/main/java/com/ruweng/training/center/RuwengTrainingCenterApplication.java
@@ -0,0 +1,11 @@
+package com.ruweng.training.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuwengTrainingCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuwengTrainingCenterApplication.class, args);
+    }
+}
